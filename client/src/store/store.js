2bea9f7229b0bcdb8b0542cc0a12eb991c1e6039
dongatuna <ngatuna05@gmail.com>
2018-09-22 13:58:16 -0700

    create components and store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,4 +3,13 @@ import Vuex from 'vuex'
 
 import event from './modules/events'
 import jobpost from './modules/jobposts'
-import resume from 
+import resume from './modules/resumes'
+import user from "./modules/users"
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    modules:{
+        event, jobpost, resume, user
+    }
+})
